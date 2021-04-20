e87a0d87bd6bcc38b61cf8d9e078dda8232022d7
Cesar Torres <>
2021-04-20 15:47:01 +0000

    Who is goin to pay the bills?

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-random_choice = random.choice(names)
-print(f"{random_choice} is going to buy the meal today!" )
+num_items = len(names)
+random = random.randint(0, num_items - 1)
+person_who_pay = names[random]
+print(person_who_pay +" is going to pay the bills")
 
 
